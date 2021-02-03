10a10bb847cc25a5d880de4b5ecad7e94a390546
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-03 11:53:59 -0600

    GroceryItem class complete

--- src/GroceryItem.java
+++ src/GroceryItem.java
@@ -1,6 +1,48 @@
+import java.util.Arrays;
+import java.util.List;
+
 public class GroceryItem {
-    private String foodItem;
+    private String name;
     private int price;
     private String category;
+
+    public String getName() {
+
+    return name;
+    }
+
+    public void setName(String foodItem) {
+        if ( name.count >2)
+            this.name = name;
+        if (name.count ("") <2)
+            throw new IllegalArgumentException(name + " needs to be more than 2 letters"+
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        if (price => 0; price =< 150)
+        this.price = price;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        if (getValidCategories().contains(category))
+            this.category = category;
+        else
+            throw new IllegalArgumentException(category + " is not a valid face name, use 1 of "+
+                    getValidCategories());
+    }
+    toString()
+
+    public static List<String> getValidCategories()
+    {
+        return Arrays.asList("vegetables", "fruit", "meat", "fish", "dairy", "bread", "condiments");
+    }
 }
 
