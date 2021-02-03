0a6e6a7f9d0768ee1f67e452783aa2f51032150e
brieannabetts <bgbetts@lakeheadu.ca>
2021-02-03 12:02:28 -0600

    Purchases instance variable and constructor complete

--- src/Purchases.java
+++ src/Purchases.java
@@ -1,2 +1,35 @@
 public class Purchases {
+    private int TotalPrice;
+    private String GroceryNames;
+    private String Categories;
+    public Purchases(int TotalPrice, String GroceryNames , String Categories)
+    {
+        setTotalPrice(TotalPrice);
+        setGroceryNames(GroceryNames);
+        setCategories(Categories);
+    }
+    public int getTotalPrice) {
+        return TotalPrice;
+    }
+
+    public void setTotalPrice(int TotalPrice) {
+        this.TotalPrice = TotalPrice;
+    }
+
+    public String getGroceryNames() {
+        return GroceryNames;
+    }
+
+    public void setGroceryNames(String GroceryNames) {
+        this.GroceryNames = GroceryNames;
+    }
+
+    public String getCategories() {
+        return Categories;
+    }
+
+    public void setCategories(String Categories) {
+        this.Categories = Categories;
+    }
+}
 }
